4ec5e48154feb64352bfa753ceb512af310f4def
Fernando Terra <fernando.terra@portoseguro.com.br>
2019-09-24 12:05:04 -0300

    Mudando

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var http = require('http');
 var handleRequest = function(request, response) {
 	console.log('Recebendo mensagem da URL...: ' + request.url);
 	response.writeHead(200);
-	response.end('Olá, Terra!');
+	response.end('Novo mensagem - Muito estranha da versao 2.0, Terra!');
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
